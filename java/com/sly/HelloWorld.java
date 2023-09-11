4669f2eadb3df20602c6447e63a5db6595e48ddd
dywss <xhtzqbx@gmail.com>
2023-09-11 23:49:01 +0800

    initial

---
+++ java/com/sly/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.sly;
+
+/**
+ * @description:
+ * @author: xiaohaotian
+ * @date: 2023/9/11 23:47
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("fuck the world");
+    }
+}
