b51a05f2dfac7e3a987d49460a640fee130fdd2f
dywss <xhtzqbx@gmail.com>
2023-09-12 00:14:27 +0800

    initial

--- java/com/sly/HelloWorld.java
+++ java/com/sly/HelloWorld.java
@@ -10,5 +10,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("fuck the world");
+        System.out.println("hellowwww");
     }
 }
